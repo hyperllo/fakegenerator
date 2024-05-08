fc0019562f7b1aa942fc37f2baa0a4664a870c00
hyperllo <113925456+hyperllo@users.noreply.github.com>
2024-05-08 09:10:37 +0400

    Update main.py

--- fakegenerator/main.py
+++ fakegenerator/main.py
@@ -1,10 +1,13 @@
 from faker import Faker
 from prettytable import PrettyTable
 from datetime import datetime, timedelta
+import random
+import string
+from transliterate import translit
 
 fake = Faker('ru_RU')
 
-table = PrettyTable(["Имя", "Адрес", "Номер кредитной карты", "Срок действия карты", "CVV", "Провайдер карты", "Дата рождения", "Возраст"])
+table = PrettyTable(["Имя", "Адрес", "Адрес почты", "Пароль", "Номер кредитной карты", "Срок действия карты", "CVV", "Провайдер карты", "Дата рождения", "Возраст"])
 
 for _ in range(5):
     city = fake.city()
@@ -17,10 +20,17 @@ for _ in range(5):
     elif age > 90:
         birth_date = datetime.now().date() - timedelta(days=90*365)
         age = 90
+    name = fake.name()
+    email_name = translit(name.split()[0], reversed=True)
+    allowed_characters = string.ascii_letters + string.digits
+    email = email_name.lower() + ''.join(random.choices(allowed_characters, k=random.randint(1, 5))) + "@" + fake.free_email_domain()
+    password = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(12, 14)))
     credit_card_number = fake.credit_card_number()
     row = [
-        fake.name(),
+        name,
         address_with_city,
+        email,
+        password,
         credit_card_number,
         fake.credit_card_expire(),
         credit_card_number[-3:],
